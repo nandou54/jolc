7e2ad92b39a89e915c8152d6da9bd4520d51634e
PabloC54 <3091865790609@ingenieria.usac.edu.gt>
2021-08-09 21:00:31 -0600

    Project organization, first publish with gh

---
+++ client/src/components/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from '@/styles/App.module.css'
+
+function App() {
+  return <p>quepex</p>
+}
+
+export default App
---
+++ client/vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [reactRefresh()],
+  base: 'client',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src')
+    }
+  }
+})

db53f950210e53c045bfcfd12b0e1132fd97896e
PabloC54 <3091865790609@ingenieria.usac.edu.gt>
2021-08-18 22:18:58 -0600

    api and frontend integratin

--- api/main.py
+++ api/main.py
@@ -1,7 +1,10 @@
-from typing import Optional
+import os
 from pydantic import BaseModel
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+from starlette.responses import FileResponse, RedirectResponse
+from starlette.templating import Jinja2Templates
+from starlette.exceptions import HTTPException
 
 from interpreter import interpret
 
@@ -14,18 +17,54 @@ class outputData(BaseModel):
 
 app = FastAPI()
 
-origins = ["http://localhost:3000"]
+# origins = ["http://127.0.0.1:8000"]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["POST"],
-    allow_headers=["*"],
-)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
+mime_types = {
+  'js':'text/javascript',
+  'css':'text/css',
+  'jpg':'image/jpeg',
+  'png':'image/png',
+  'svg':'image/svg+xml',
+  'txt':'text/plain'
+}
+
+templates = Jinja2Templates(directory="../dist")
 
 @app.post("/api/")
 def analyze_input(input: inputData):
   result = interpret(input.text)
   return result
+
+@app.get("/client")
+async def serve_app(request: Request):
+  return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/client/static/{filename}")
+async def serve_file(request: Request, filename):
+  file_path ='../dist/static/'+filename
+
+  if not os.path.exists(file_path):
+    return {"error":"File not found"}
+
+  file_array = filename.split('.')
+
+  extension = 'txt'
+  if len(file_array)>1:
+    extension = file_array[-1].lower()
+
+  if extension not in mime_types.keys():
+    extension = 'txt'
+
+  return FileResponse(file_path, media_type=mime_types[extension])
+
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request, exc):
+    return RedirectResponse("/client")

8b3b16fae350ef871debeaebcc936b96b84538a2
nanndo54 <nanndo54@outlook.com>
2024-01-04 22:48:51 -0600

    Remove unnecessary files and update main.py

--- api/main.py
+++ api/main.py
@@ -1,3 +1,5 @@
+import uvicorn
+
 from pydantic import BaseModel
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -37,3 +39,6 @@ def analyze_input(input_data: InputData):
 @app.post('/api/optimize/blocks')
 def analyze_input(input_data: InputData):
   return optimize_blocks(input_data.content)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
